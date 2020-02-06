83887f02225ecc2b81179de48bde832acb2f3c38
sgneha <sgneha05@gmail.com>
2020-02-06 21:12:57 +0000

    step8 Exercise 4

--- directory_copy.rb
+++ directory_copy.rb
@@ -22,8 +22,10 @@ def print_header
   puts "--------------"
 end
 def print(students)
-  students.each do |student|
-    puts "#{student[:name]} (#{student[:cohort]} cohort)" if student[:name].length < 12
+  count = 0
+  while count < students.length
+    puts "#{students[count][:name]} (#{students[count][:cohort]} cohort)"
+    count+=1
   end
 end
 def print_footer(names)
