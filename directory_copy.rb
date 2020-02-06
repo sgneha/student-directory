337c48fa12efe1c8d912bd8307782d743b023dfd
sgneha <sgneha05@gmail.com>
2020-02-06 20:47:41 +0000

    step8 Exercise 2

--- directory_copy.rb
+++ directory_copy.rb
@@ -23,7 +23,7 @@ def print_header
 end
 def print(students)
   students.each_with_index do |student,index|
-    puts "#{index+1}. #{student[:name]} (#{student[:cohort]} cohort)"
+    puts "#{index+1}. #{student[:name]} (#{student[:cohort]} cohort)" if student[:name][0].upcase =='A'
   end
 end
 def print_footer(names)
