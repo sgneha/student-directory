35016caed70c6b362456fec6842e7df7e30d77f7
sgneha <sgneha05@gmail.com>
2020-02-13 20:53:42 +0000

    step 9 Interactive Menu

--- directory_copy.rb
+++ directory_copy.rb
@@ -1,8 +1,30 @@
-def input_students
-  puts "Please enter details of the students".center(50)
+
+def interactive_menu
+  students = []
+  loop do
+    puts "1. Input the students"
+    puts "2. Show the students"
+    puts "9. Exit"
+    selection = gets.chomp
+    case selection
+    when "1"
+      students = input_students(students)
+    when "2"
+      print_header
+      print(students)
+      print_footer(students)
+    when "9"
+      exit
+    else
+      puts "I don't know what you meant, try again"
+    end
+  end
+end
+	def input_students(students)
+  # puts "Please enter details of the students".center(50)
   puts "To finish, just hit return twice".center(50)
   #create an empty array
-  students = []
+  # students = []
   while true do
     puts "Name of the student"
     name = gets.strip
@@ -10,17 +32,17 @@ def input_students
     if name.empty?
       break
     end
-    puts "Hobby"
-    hobby = gets.strip
-    puts"Height of the student"
-    height = gets.chop
-    puts"Country"
-    country = gets.chop
+    # puts "Hobby"
+    # hobby = gets.strip
+    # puts"Height of the student"
+    # height = gets.chop
+    # puts"Country"
+    # country = gets.chop
     puts"Cohort"
     cohort = gets.chop
     cohort = "January" if cohort == ""
     #add the student hash to the array
-    students<<{name: name,hobby: hobby, height: height, country: country, cohort: cohort.to_sym}
+    students<<{name: name, cohort: cohort.to_sym}
     if students.length ==1
       puts "Now we have 1 student"
       else
@@ -38,14 +60,15 @@ end
 def print(students)
 # puts "Specify Cohort or All"
 # input = gets.chomp
- students.sort_by!{|student| student[:cohort]}
- prev_cohort = ''
+  students.sort_by!{|student| student[:cohort]}
+  prev_cohort = ''
   students.each do|student|
     if prev_cohort != student[:cohort]
        prev_cohort = student[:cohort]
        puts "#{student[:cohort]} Cohort"
     end
-    puts "Name: #{student[:name]}, Hobby: #{student[:hobby]}, Height: #{student[:height]}, Country: #{student[:country]}"
+    puts "Name: #{student[:name]}"
+  end
 end
   # students.each do|student|
   #   if input.to_sym == student[:cohort]
@@ -59,7 +82,7 @@ end
 #    puts "Name: #{students[count][:name]}, #{students[count][:cohort]} Cohort, Hobby: #{students[count][:hobby]}, Height: #{students[count][:height]}, Country: #{students[count][:country]}"
 #    count+=1
 #  end
-end
+
 def print_footer(names)
   if names.count == 1
     puts "Overall, we have #{names.count} great student".center(50)
@@ -67,12 +90,10 @@ def print_footer(names)
     puts "Overall, we have #{names.count} great students".center(50)
   end
 end
+interactive_menu
 #nothing happens until we call the methods
-students = input_students
+
 # if students.count == 0
 #   puts "There are no students in the List"
 #   exit
 # end
-print_header
-print(students)
-print_footer(students)
