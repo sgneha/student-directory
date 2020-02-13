cd853f8456587e43abc5d2ad5f979bc6e24dfe16
sgneha <sgneha05@gmail.com>
2020-02-13 23:27:33 +0000

    step 12 Loading data from File

--- directory.rb
+++ directory.rb
@@ -1,6 +1,5 @@
 @students = []
 def interactive_menu
-  # students = []
   loop do
     print_menu
     process(gets.chomp)
@@ -10,6 +9,7 @@ def print_menu
     puts "1. Input the students"
     puts "2. Show the students"
     puts "3. Save the list to students.csv "
+    puts "4. Load the list from students.csv"
     puts "9. Exit"
 end
 def process(selection)
@@ -20,6 +20,8 @@ def process(selection)
       show_students
     when "3"
       save_students
+    when "4"
+      load_students
     when "9"
       exit
     else
@@ -41,12 +43,6 @@ def input_students
     if name.empty?
       break
     end
-    # puts "Hobby"
-    # hobby = gets.strip
-    # puts"Height of the student"
-    # height = gets.chop
-    # puts"Country"
-    # country = gets.chop
     puts"Cohort"
     cohort = gets.chop
     cohort = "January" if cohort == ""
@@ -78,9 +74,7 @@ def print_footer
   end
 end
 def save_students
-  # open the file for writing
   file = File.open("students.csv", "w")
-  # iterate over the array of students
   @students.each do |student|
     student_data = [student[:name], student[:cohort]]
     csv_line = student_data.join(",")
@@ -88,36 +82,12 @@ def save_students
   end
   file.close
 end
+def load_students
+  file = File.open("students.csv","r")
+  file.readlines.each do |line|
+    name, cohort = line.chomp.split(',')
+    @students << {name: name, cohort: cohort.to_sym}
+  end
+  file.close
+end
 interactive_menu
-
-
-# puts "Specify Cohort or All"
-# input = gets.chomp
-  # students.sort_by!{|student| student[:cohort]}
-  # prev_cohort = ''
-  # students.each do|student|
-  #   if prev_cohort != student[:cohort]
-  #      prev_cohort = student[:cohort]
-  #      puts "#{student[:cohort]} Cohort"
-  #   end
-  #   puts "Name: #{student[:name]}"
-  # end
-
-  # students.each do|student|
-  #   if input.to_sym == student[:cohort]
-  #     puts "Name: #{student[:name]}, Hobby: #{student[:hobby]}, Height: #{student[:height]}, Country: #{student[:country]}"
-  #   elsif input == "All"
-  #     puts "Name: #{student[:name]}, Hobby: #{student[:hobby]}, Height: #{student[:height]}, Country: #{student[:country]}, #{student[:cohort]} Cohort"
-  #   end
-  # end
-#  count = 0
-#  while count < students.length
-#    puts "Name: #{students[count][:name]}, #{students[count][:cohort]} Cohort, Hobby: #{students[count][:hobby]}, Height: #{students[count][:height]}, Country: #{students[count][:country]}"
-#    count+=1
-#  end
-#nothing happens until we call the methods
-
-# if students.count == 0
-#   puts "There are no students in the List"
-#   exit
-# end
